32d919d429ab11025d83eaecd2d7bdd99c5862d3
Eric P <cire.ttalp@gmail.com>
2024-08-27 23:56:05 -0700

    Add a line with an Column layout

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
@@ -21,10 +25,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    Column {
+                        Banner(
+                            modifier = Modifier.padding(innerPadding).background(
+                                color = Color.Cyan))
+                        Greeting(
                         name = "Eric",
                         modifier = Modifier.padding(innerPadding)
                     )
+                    }
                 }
             }
         }
@@ -35,6 +44,14 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = stringResource(R.string.hi_my_name_is, name),
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Banner(modifier: Modifier = Modifier) {
+    Text(
+        text = stringResource(R.string.welcome_to_the_first_of_many_android_apps),
         modifier = modifier
     )
 }
