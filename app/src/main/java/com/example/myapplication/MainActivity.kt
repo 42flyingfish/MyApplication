5e1c84799aeda40a047e868ea0462d8fa474b969
Eric P <cire.ttalp@gmail.com>
2024-08-27 23:44:20 -0700

    Use compose to implement a button.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.StackView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
@@ -21,15 +30,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Eric",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                    Clicky(modifier = Modifier.padding(innerPadding))
             }
         }
     }
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -39,10 +45,33 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun Clicky(modifier: Modifier) {
+    var wasIClicked by remember { mutableStateOf(false) }
+    
+    Column {
+        if (wasIClicked) {
+            Text(
+                text = stringResource(R.string.button_clicked),
+                modifier = modifier
+            )
+        } else {
+            Greeting(
+                name = stringResource(R.string.eric),
+                modifier = modifier
+            )
+        }
+        Button(onClick = { wasIClicked = true }) {
+            Text(text = stringResource(R.string.click_me))
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
         Greeting("Android")
     }
+}
 }
